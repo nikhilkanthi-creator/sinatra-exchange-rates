a526ad3b032dc4b3ad6d68a1a3f1ceaf843360d8
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-22 03:18:29 +0000

    second page construction

--- app.rb
+++ app.rb
@@ -12,3 +12,14 @@ get("/") do
   @currencies = @parsed_response.fetch("currencies")
   erb(:homepage)
 end
+
+get("/:first_symbol") do
+  @symbol = params.fetch("first_symbol")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  @currencies = @parsed_response.fetch("currencies")
+
+erb(:step_one)
+end
