3c31bd2042a11fe98011d2972cbe18e80df0cfd7
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-22 03:54:45 +0000

    i think i'm done/???

--- app.rb
+++ app.rb
@@ -27,4 +27,11 @@ end
 get("/:first_symbol/:second_symbol") do
   @first_symbol = params.fetch("first_symbol")
   @second_symbol = params.fetch("second_symbol")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/convert?from=#{@first_symbol}&to=#{@second_symbol}&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  @value1 = @parsed_response.fetch("info")
+  @value2 = @value1.fetch("quote")
+  erb(:step_two)
 end
